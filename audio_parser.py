d64af6afa736556d5e99cbad04e380c8d396d851
Michael Yang <michaelyang@Michaels-MacBook-Air.local>
2025-05-20 00:02:31 -0700

    Complete data parsing

--- audio_parser.py
+++ audio_parser.py
@@ -2,7 +2,7 @@ import audiofile
 import opensmile
 import os
 import pandas as pd
-
+import numpy as np
 def extract_audio_features_from_recordings_directory(recordings_directory, save_to_file=False):
     smile = opensmile.Smile(
         feature_set=opensmile.FeatureSet.GeMAPS,
@@ -27,16 +27,44 @@ def extract_audio_features_from_recordings_directory(recordings_directory, save_
     return features_list
 
 def get_inputs_from_features_directory(features_directory):
-    inputs = []
+    inputs_dict = {}
     for filename in os.listdir(features_directory):
+        speaker_id = filename[:7]
         feature_sample = pd.read_csv(os.path.join(features_directory, filename))
-        inputs.append(feature_sample)
         print(filename)
-    return inputs
+        if speaker_id not in inputs_dict:
+            inputs_dict[speaker_id] = [feature_sample]
+        else:
+            inputs_dict[speaker_id].append(feature_sample)
+    return inputs_dict
 
 def get_targets_from_targets_directory(filename):
-    targets = pd.read_csv(filename)
-    return targets
+    target_data = pd.read_csv(filename)
+    targets_dict = {}
+    for _, row in target_data.iterrows():
+        speaker_id = f"spk_{int(row['Participant_ID'])}"        
+        phq8_binary = row['PHQ8_Binary']
+        phq8_score = row['PHQ8_Score']
+        
+        # phq8_binary is 1 if phq8_score >= 10 else 0
+        # print(speaker_id, phq8_binary, phq8_score)
+        targets_dict[speaker_id] = pd.DataFrame([phq8_score], columns=['PHQ8_Score'])
+    return targets_dict
 
-def match_features_to_targets(features, targets):
-    pass
+def match_input_features_to_targets(inputs_dict, targets_dict):
+    # we need this function because there are ~150 input features for each target
+    # ex: there are 150 audio recordings for a specific speaker, but that speaker only produces one depression outcome (i.e. many to one)
+    x = []
+    y = []
+    for speaker_id in inputs_dict.keys():
+        speaker_inputs = inputs_dict[speaker_id] # list of many dataframes
+        speaker_target = targets_dict[speaker_id] # list of single dataframe
+        
+        for input in speaker_inputs:
+            x.append(input)
+            y.append(speaker_target)
+    
+    x = np.array(x)
+    y = np.array(y)
+    print(x.shape, y.shape)
+    return x, y
--- main.py
+++ main.py
@@ -5,11 +5,11 @@ if __name__ == "__main__":
     # features = audio_parser.extract_audio_features_from_recordings_directory(recordings_directory, save_to_file=False)
 
     features_directory = "audio_extracted_features.nosync"
-    # inputs = audio_parser.get_inputs_from_features_directory(features_directory)
+    inputs = audio_parser.get_inputs_from_features_directory(features_directory)
     # print(len(inputs))
     # print(inputs[0].shape)
 
     targets_filename = "train_split_Depression_AVEC2017.csv"
     targets = audio_parser.get_targets_from_targets_directory(targets_filename)
-    print(targets.shape)
-    print(targets.head())
+    
+    audio_parser.match_input_features_to_targets(inputs, targets)
